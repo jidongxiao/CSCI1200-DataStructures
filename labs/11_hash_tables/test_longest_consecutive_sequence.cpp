f673ce2caad02db7e9f06b2192a4e40aaba9a627
Jidong Xiao <xiaoj8@rpi.edu>
2023-11-08 10:05:41 -0500

    adding negative test case

--- labs/11_hash_tables/test_longest_consecutive_sequence.cpp
+++ labs/11_hash_tables/test_longest_consecutive_sequence.cpp
@@ -10,6 +10,7 @@ int main() {
 	//std::vector<int> nums = {100, 4, 200, 1, 3, 2, 5, 6};
 	//std::vector<int> nums = {0,3,7,2,5,8,4,6,0,1};
 	//std::vector<int> nums = {100, 4, 200, 201, 202, 203, 205, 204, 1, 3, 2};
+	//std::vector<int> nums = {-3,0,1,2,3,-2,-1,-5};
 	int size = nums.size();
 	std::cout<< "for vector {";
 	for(int i=0;i<size-1;i++){

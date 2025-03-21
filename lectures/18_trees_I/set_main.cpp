33af439c32f52c3e0c5701fc0f1e8183b69c6499
Jidong Xiao <xiaoj8@rpi.edu>
2025-03-20 23:59:15 -0400

    create set main and ds set main

--- lectures/18_trees_I/set_main.cpp
+++ lectures/18_trees_I/set_main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "ds_set_starter.h"
+#include <set>
 
 int main() {
     // create a set of integers
-    ds_set<int> numbers;
+    std::set<int> numbers;
 
     // insert some values into the set
     numbers.insert(10);
@@ -12,9 +12,16 @@ int main() {
     numbers.insert(15);
     numbers.insert(5); // duplicate value (won't be inserted)
 
+    // print the elements of the set
+    std::cout << "The elements in the set are:" << std::endl;
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
     // check if a specific value exists in the set
     int value = 15;
-    if (numbers.find(value) == true) {
+    if (numbers.find(value) != numbers.end()) {
         std::cout << value << " is found in the set." << std::endl;
     } else {
         std::cout << value << " is not found in the set." << std::endl;

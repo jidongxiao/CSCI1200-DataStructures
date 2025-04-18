556bab4bf96f8a51a377e28f8c383a28a442a6e0
Jidong Xiao <xiaoj8@rpi.edu>
2025-04-18 03:00:08 -0400

    adding shared pointer example

---
+++ lectures/27_garbage_collection/shared_ptr1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <memory>
+
+int main(){
+	std::shared_ptr<int> age(new int(40));
+	std::cout << "age is " << *age << std::endl;
+
+	// you can never do this, which is assigning a smarter pointer to a raw pointer.
+	// int * temp = age;
+	
+	{
+		std::shared_ptr<int> temp = age;
+		std::cout << "age is " << *temp << std::endl;
+		std::cout << "the use count is : " << age.use_count() << std::endl;
+	}
+	std::cout << "the use count is : " << age.use_count() << std::endl;
+
+	// give up my ownership, it decreases the reference count of the managed object by one.
+	// if that shared pointer was the last owner (i.e., reference count becomes zero), the object is deleted. 
+	// the shared_ptr itself is now empty (i.e., it holds nullptr).
+	age.reset();
+	std::cout << "the use count is : " << age.use_count() << std::endl;
+}
